a27dc748bb78072b14da6b897f78d4a53bc17d1c
Alexander Kovtun <akovtun@gmail.com>
2020-02-20 14:14:12 +0200

    create main navigation container

---
+++ src/navigation/index.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+
+import HomeScreen from '../screens/home';
+type HomeStackParamList = {
+  Home: {someProps: string};
+};
+
+const HomeStack = createStackNavigator<HomeStackParamList>();
+const HomeStackNavigator = () => (
+  <HomeStack.Navigator>
+    <HomeStack.Screen name="Home" component={HomeScreen} />
+  </HomeStack.Navigator>
+);
+
+export default () => (
+  <NavigationContainer>
+    <HomeStackNavigator />
+  </NavigationContainer>
+);
